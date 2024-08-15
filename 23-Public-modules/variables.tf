51f07a549f895fbf9b05867fc58f085c5e390260
Kasun Rajapakse <16514562+kasunsjc@users.noreply.github.com>
2024-08-15 22:22:40 +0200

    Add Terraform public modules

--- 23-Public-modules/variables.tf
+++ 23-Public-modules/variables.tf
@@ -1,48 +1,45 @@
-# Input Variables
 variable "business_unit" {
   description = "Business Unit Name"
-  type = string
-  default = "hr"
+  type        = string
+  default     = "it"
 }
 variable "environment" {
   description = "Environment Name"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 variable "resoure_group_name" {
   description = "Resource Group Name"
-  type = string
-  default = "myrg"
+  type        = string
+  default     = "rg"
 }
 variable "resoure_group_location" {
   description = "Resource Group Location"
-  type = string
-  default = "eastus"
+  type        = string
+  default     = "southeastasia"
 }
 variable "virtual_network_name" {
   description = "Virtual Network Name"
-  type = string 
-  default = "myvnet"
+  type        = string
+  default     = "vnet"
 }
 variable "subnet_name" {
   description = "Virtual Network Subnet Name"
-  type = string 
+  type        = string
 }
-variable "publicip_name" {
+variable "public_ip_name" {
   description = "Public IP Name"
-  type = string 
+  type        = string
 }
 variable "network_interface_name" {
   description = "Network Interface Name"
-  type = string 
+  type        = string
 }
 
-variable "virtual_machine_name" {
-  description = "Virtual Machine Name"
-  type = string 
+variable "use_for_each" {
+  type    = bool
+  default = true
 }
-
-
 
 
 
